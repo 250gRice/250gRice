d67ab360b162fd27ef056e4f147d5be155c0638e
MarcoMaar <marcovic@student.tugraz.at>
2013-04-24 10:17:53 +0200

    OPEN - task 25: Refactor Page http://github.com/250gRice/250gRice/issues/issue/25

--- 250gRice/src/sw/app_250grice/Page.java
+++ 250gRice/src/sw/app_250grice/Page.java
@@ -21,7 +21,7 @@ public class Page {
 		try {
 			toSearch = getItemByNameAndUnit(toAdd.getName(), toAdd.getUnit());
 		} catch (ItemNotFoundException e) {
-			items.add(toAdd.clone());
+			items.add(toAdd.clone()123);
 			return;
 		}
 		
