9e196ae09a08dfecbdc5f6a3a5d354a5ceb423d3
fsteinbauer <steinbauer.florian@gmail.com>
2013-05-29 18:31:45 +0200

    - Add Page - Add Item

--- 250gRice/src/sw/app_250grice/PageActivity.java
+++ 250gRice/src/sw/app_250grice/PageActivity.java
@@ -5,16 +5,23 @@ import java.util.List;
 
 import sw.app_database.DatabaseHelper;
 import sw.app_database.DatabaseManager;
+import sw.commands.CommandAddBlankPage;
+import sw.commands.CommandAddItemToPageByName;
+import sw.commands.ICommand;
+import sw.exceptions.PageNameAlreadyExistsException;
 import sw.exceptions.PageNotFoundException;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.support.v4.app.NavUtils;
 import android.annotation.TargetApi;
 import android.content.Intent;
@@ -26,7 +33,7 @@ public class PageActivity extends Activity {
 	public final static String PAGEID = "sw.app_250grice.PAGEID";
 	PageHandler pageHandler;
 	Page currentPage;
-
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -39,13 +46,33 @@ public class PageActivity extends Activity {
 		Intent intent = getIntent();
 		int pageId = intent.getIntExtra(PAGEID, -1);
 		
-		try {
-			currentPage = pageHandler.getPageById(pageId);
-		} catch (PageNotFoundException e) {
-			System.err.println("ERRORERRORERRORERRORERRORERRORERRORERRORERRORERRORERRORERRORERRORERRORERRORERROR" + pageId);
-			e.printStackTrace();
-			return;
+		System.out.println("Pageid: "+pageId);
+		
+		// Create a blank Page
+		if( pageId == R.id.blank_page){
+			
+			setTitle(R.string.untiled_page);
+			currentPage.setName(getString(R.string.untiled_page));
+			
+			
+			try {
+				ICommand addBlankPageCommand = new CommandAddBlankPage(getString(R.string.untiled_page));
+				addBlankPageCommand.execute();
+			} catch (PageNameAlreadyExistsException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			System.out.println("Newpage clicked");
+		} else {
+			try {
+				currentPage = pageHandler.getPageById(pageId);
+			} catch (PageNotFoundException e) {
+				e.printStackTrace();
+				return;
+			}
 		}
+		
 		
 		setContentView(createView());
 	}
@@ -53,30 +80,39 @@ public class PageActivity extends Activity {
 	
 	private TableLayout createView(){
 		
-		List<Item> items = currentPage.getItems();
 		TableLayout pageLayout = (TableLayout) getLayoutInflater().inflate(R.layout.overview_page_layout, null);
-		
-		System.out.println(items.size());
-		
-		for(Item item: items){
-			TableRow itemLayout = new TableRow(this);
+		if(currentPage != null){
+			List<Item> items = currentPage.getItems();
 			
-			TextView 	qty =	new TextView(this),
-						name =	new TextView(this);
 			
-			DecimalFormat f = new DecimalFormat("#0.00 ");
-			String value = f.format(item.getValue());
-			qty.setText(value+item.getUnit().toString());
-			qty.setPadding(15, 15, 0, 0);
-			
-			name.setText(item.getName());
-			name.setPadding(15, 15, 15, 0);
-			
-			itemLayout.addView(qty);
-			itemLayout.addView(name);					
-			
-			pageLayout.addView(itemLayout);
+			if(items != null && items.size() != 0){
+				
+				
+				for(Item item: items){
+					TableRow itemLayout = new TableRow(this);
+					
+					TextView 	qty =	new TextView(this),
+								name =	new TextView(this);
+					
+					DecimalFormat f = new DecimalFormat("#0.00 ");
+					String value = f.format(item.getValue());
+					qty.setText(value+item.getUnit().toString());
+					qty.setPadding(15, 15, 0, 0);
+					
+					name.setText(item.getName());
+					name.setPadding(15, 15, 15, 0);
+					
+					itemLayout.addView(qty);
+					itemLayout.addView(name);					
+					
+					pageLayout.addView(itemLayout);
+				}
+			}
 		}
+		
+		LinearLayout newItemEdit = (LinearLayout) getLayoutInflater().inflate(R.layout.add_new_item, null);
+		pageLayout.addView(newItemEdit);
+			
 
 		return pageLayout;
 	
@@ -91,6 +127,25 @@ public class PageActivity extends Activity {
 		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
 			getActionBar().setDisplayHomeAsUpEnabled(true);
 		}
+	} 
+	
+	
+	
+	public void addItem(View view) {
+			
+		EditText editText = (EditText) findViewById(R.id.text_add_item);
+		String textInput = editText.getText().toString();
+		Item toAdd = new Item(textInput, 1);
+		try {
+			ICommand addItemCommand = new CommandAddItemToPageByName(toAdd, currentPage.getName());
+			addItemCommand.execute();
+		} catch (PageNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		Toast toast = Toast.makeText(this, textInput, Toast.LENGTH_SHORT);
+		toast.show();
 	}
 
 	@Override
